ac5a810d4eac40b10d5d1aae656aa8e0076c49bb
TrickerMadness <39390723+TrickerMadness@users.noreply.github.com>
2018-05-17 21:15:45 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,15 +6,16 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === '^assassination classroom') {
+    var msg = message.content.toLowerCase();
+    if (msg === '^assassination classroom') {
       message.reply('https://www.watchcartoononline.io/anime/assassination-classroom-english-subbed');
-    } else if (message.content === '^blend s') {
+    } else if (msg === '^blend s') {
       message.reply('https://https://www.watchcartoononline.io/anime/blend-s-english-subbed');
-    } else if (message.content === '^re:zero') {
+    } else if (msg === '^re:zero') {
       message.reply('https://www.watchcartoononline.io/anime/re-zero-kara-hajimeru-isekai-seikatsu-english-subbed');
-    } else if (message.content === '^help') {
+    } else if (msg === '^help') {
       message.reply('To use this bot please enter the name of an anime with ^ in front of it and if we have that anime link it will');
-    } else if (message.content === '^List') {
+    } else if (msg === '^list') {
       message.reply('assassination classroom, blend s, re:zero ');
     }
 });
